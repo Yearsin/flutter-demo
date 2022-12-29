a697d02e6c9bd61315a1f8b68cfcbdd7e689df61
qinlicang <18303033133@163.com>
2022-12-29 19:57:37 +0800

    feat: buju

--- lib/main.dart
+++ lib/main.dart
@@ -20,13 +20,32 @@ class MyApp extends StatelessWidget {
           title: const Text('标题'),
         ),
         body: Row(
-          crossAxisAlignment: CrossAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
+            Container(
+              alignment: Alignment.center,
+              width: 200.0,
+              padding: const EdgeInsets.only(left: 10.0, right: 10.0),
+              child: Column(
+                children: const [
+                  Text('title', style: TextStyle(fontSize: 28.0, color: Colors.black87)),
+                  Text('sdfsfsfsdfsdfsdsdfsfsfsdfsdfsdsdfsfsfsdfsdfsdsdfsfsfsdfsdfsdsdfsfsfsdfsdfsdsdfsfsfsdfsdfsdsdfsfsfsdfsdfsd', textAlign: TextAlign.center, softWrap: true, style: TextStyle(fontSize: 14, color: Colors.black38)),
+                ],
+              ),
+            ),
             Expanded(child: Image.asset('images/1.png')),
-            Expanded(flex: 2, child: Image.asset('images/2.png')),
-            Expanded(child: Image.asset('images/3.png'))
           ],
         ),
+        // 使用flex使图片突出
+        // Row(
+        //   crossAxisAlignment: CrossAxisAlignment.center,
+        //   children: [
+        //     Expanded(child: Image.asset('images/1.png')),
+        //     Expanded(flex: 2, child: Image.asset('images/2.png')),
+        //     Expanded(child: Image.asset('images/3.png'))
+        //   ],
+        // ),
+        // 星星
         // Row(
         //   mainAxisSize: MainAxisSize.min,
         //   children: [
